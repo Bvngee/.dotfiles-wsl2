c02e9d0c1d408fe24ba454026b7173139850791c
BvngeeCord <nystromjp@gmail.com>
2022-10-09 15:22:54 -0700

    more

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 sudo apt update && apt upgrade -y
 
-sudo apt install -y git zsh starship ranger curl stow
+sudo apt install -y zsh ranger curl stow neofetch
+
+curl -sS https://starship.rs/install.sh | sh
+
+
+
+#git submodule initialization
+git submodule update --init
