f10e00e76a1fa8061f2ff61412d538ad672aa373
BvngeeCord <nystromjp@gmail.com>
2022-10-15 17:13:49 -0700

    tad bit of install script stuff, much more to come

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 sudo apt update && apt upgrade -y
 
-sudo apt install -y zsh ranger curl stow neofetch
+sudo apt install -y zsh ranger curl stow neofetch zip unzip
+
+sudo chsh -s $(which zsh) $(whoami)
 
 curl -sS https://starship.rs/install.sh | sh
-
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts && nvm use --lts
 
 #git submodule initialization
 git submodule update --init
